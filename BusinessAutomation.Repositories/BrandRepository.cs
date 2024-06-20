47d1ac123d68ffed46bcc9f708f08bd89517af0c
Deb <debprosadbaidya20@gmail.com>
2024-06-20 22:28:04 +0600

    Add project files.

---
+++ BusinessAutomation.Repositories/BrandRepository.cs
@@ -0,0 +1,44 @@
+﻿using BusinessAutomation.Database;
+using BusinessAutomation.Models.EntityModels;
+using BusinessAutomation.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAutomation.Repositories
+{
+    public class BrandRepository: BaseRepository<Brand>
+    {
+        BusinessAutomationDbContext db;
+        public BrandRepository(BusinessAutomationDbContext db):base(db)
+        {
+            this.db = db;
+          
+        }
+
+        public BusinessAutomationDbContext Db
+        {
+            get
+            {
+                return db;
+            }
+            set
+            {
+                db = value;
+            }
+        }
+
+        public Brand GetById(int id)
+        {
+            var existingBrand = db.Brands.FirstOrDefault(c => c.Id == id);
+
+            return existingBrand;
+        }
+
+      
+    
+    
+    }
+}
